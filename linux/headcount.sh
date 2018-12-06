106cdefda70accb11dd937ab9d3181097cee667b
Scott <saf6260@rit.edu>
2018-12-06 12:46:21 -0500

    Adding comments to scripts

--- linux/headcount.sh
+++ linux/headcount.sh
@@ -1,20 +1,26 @@
+#System for notifying mentors about headcount
+#Author Scott Frauenknecht saf6260
 echo 'headcout system starting'
 
+#Fuction for sending info to console and for opening the windows
 notify-headcount() {
   echo 'complete headcount now'
   google-chrome index.html
   google-chrome http://sse.rit.edu/go/headcount
 }
 
-hour=$(date +'%I')
-
+#Running the loop of updates until the user closes the bash window
 while true; do
 now=$(date +'%M')
 echo $now
 
+#Checking to see if the number of seconds is at 30
+#If so, calls the function and then sleeps for 1 hour
 if (($now == 30)); then
   echo 'calling notify-headcount'
   notify-headcount & sleep 3600;
+#Now the system will attempt to find how much time there is til :30 min
+#Once found, the system sleeps for that many minutes
 elif (($now != 30)); then
   if (($now < 45)); then
     ((waitTime=30-$now))
--- windows/headcount.sh
+++ windows/headcount.sh
@@ -1,5 +1,8 @@
+#System to alert mentor on duty to complete headcount
+#Author Scott Frauenknecht saf6260 
 echo 'headcout system starting'
 
+#Function for notifying the mentor to do headcount
 notify-headcount() {
   echo 'complete headcount now'
   start chrome http://www.youtube.com/watch?v=dQw4w9WgXcQ
@@ -7,13 +10,17 @@ notify-headcount() {
   start chrome file:///Z:/profile.V2/Documents/headcount/index.html
 }
 
+#Runs until the system is shut down by the user
 while true; do
 now=$(date +'%M')
 echo $now
 
+#Checks to see if the sysem is at 30 minutes
+#If so, the function is called and the system sleeps for one hour
 if (($now == 30)); then
   echo 'calling notify-headcount'
   notify-headcount & sleep 3600
+#If its not :30, system calculates how many minutes til :30 and then sleeps
 elif (($now != 30)); then
   if (($now < 30)); then
     ((waitTime=30-$now))
